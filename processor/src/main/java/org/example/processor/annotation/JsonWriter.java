fe4c66f72977881d676c05b515bbfa898b720a93
benjamin.slabbert <benjamin.slabbert@gmail.com>
2023-02-19 14:33:38 +0200

    ignoring target

--- processor/src/main/java/org/example/processor/annotation/JsonWriter.java
+++ processor/src/main/java/org/example/processor/annotation/JsonWriter.java
@@ -7,5 +7,4 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.SOURCE)
-public @interface JsonWriter {
-}
+public @interface JsonWriter {}
--- processor/target/test-classes/Example.java
+++
@@ -1,8 +0,0 @@
-package my.test;
-
-import org.example.processor.annotation.JsonWriter;
-import java.util.List;
-
-@JsonWriter(name = "example")
-public record Example (String name) {
-}
