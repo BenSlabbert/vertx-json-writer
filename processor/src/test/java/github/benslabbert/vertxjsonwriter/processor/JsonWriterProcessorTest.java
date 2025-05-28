98443b28b6c72853db41f68dbee032fcda6c6477
benjamin.slabbert <benjamin.slabbert@gmail.com>
2025-05-28 20:38:52 +0200

    adding tests

--- processor/src/test/java/github/benslabbert/vertxjsonwriter/processor/JsonWriterProcessorTest.java
+++ processor/src/test/java/github/benslabbert/vertxjsonwriter/processor/JsonWriterProcessorTest.java
@@ -7,24 +7,20 @@ import static com.google.common.truth.Truth.assertThat;
 import com.google.testing.compile.JavaFileObjects;
 import com.google.testing.compile.JavaSourceSubjectFactory;
 import java.net.URL;
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class JsonWriterProcessorTest {
 
-  @Test
-  void example() {
-    URL resource = this.getClass().getClassLoader().getResource("Example.java");
-    assertThat(resource).isNotNull();
-
-    assertAbout(JavaSourceSubjectFactory.javaSource())
-        .that(JavaFileObjects.forResource(resource))
-        .processedWith(new JsonWriterProcessor())
-        .compilesWithoutError();
+  static Stream<String> source() {
+    return Stream.of("Example.java", "Nested.java", "Primitive.java");
   }
 
-  @Test
-  void nested() {
-    URL resource = this.getClass().getClassLoader().getResource("Nested.java");
+  @ParameterizedTest
+  @MethodSource("source")
+  void example(String className) {
+    URL resource = this.getClass().getClassLoader().getResource(className);
     assertThat(resource).isNotNull();
 
     assertAbout(JavaSourceSubjectFactory.javaSource())
