f7d7d48c948342985ba61c66d6990de176a07407
benjamin.slabbert <benjamin.slabbert@gmail.com>
2025-05-29 15:01:30 +0200

    fix compile-test import

--- processor/src/main/java/github/benslabbert/vertxjsonwriter/processor/ToJsonGenerator.java
+++ processor/src/main/java/github/benslabbert/vertxjsonwriter/processor/ToJsonGenerator.java
@@ -11,6 +11,7 @@ final class ToJsonGenerator {
 
   private ToJsonGenerator() {}
 
+  // todo: make null safe
   static void toJson(PrintWriter out, List<Property> properties, String simpleClassName) {
     simpleClassName = simpleClassName.replace('_', '.');
     out.printf("public static JsonObject toJson(%s %s) {%n", simpleClassName, "o");
@@ -27,16 +28,17 @@ final class ToJsonGenerator {
         out.printf("json.put(\"%s\", o.%s());%n", name, name);
         continue;
       }
+
       if (className.startsWith("java.lang.")) {
         out.printf("json.put(\"%s\", o.%s());%n", name, name);
       } else if (className.startsWith("java.time.")) {
-        timeToJson(out, property.name(), property.className());
+        timeToJson(out, property.name(), property.className(), property.nullable());
       } else if (className.startsWith("java.util.Set")) {
-        iterableToJson(out, property.name(), property.className());
+        iterableToJson(out, property.name(), property.className(), property.nullable());
       } else if (className.startsWith("java.util.List")) {
-        iterableToJson(out, property.name(), property.className());
+        iterableToJson(out, property.name(), property.className(), property.nullable());
       } else if (className.startsWith("java.util.Collection")) {
-        iterableToJson(out, property.name(), property.className());
+        iterableToJson(out, property.name(), property.className(), property.nullable());
       } else {
         out.printf("json.put(\"%s\", o.%s().toJson());%n", name, name);
       }
@@ -47,23 +49,53 @@ final class ToJsonGenerator {
     out.println();
   }
 
-  private static void timeToJson(PrintWriter out, String name, String className) {
+  private static void timeToJson(PrintWriter out, String name, String className, boolean nullable) {
     switch (className) {
-      case "java.time.LocalDate" ->
+      case "java.time.LocalDate" -> {
+        if (nullable) {
+          out.printf(
+              "json.put(\"%s\", null == o.%s() ? null :"
+                  + " o.%s().format(DateTimeFormatter.ISO_DATE));%n",
+              name, name, name);
+        } else {
           out.printf("json.put(\"%s\", o.%s().format(DateTimeFormatter.ISO_DATE));%n", name, name);
-      case "java.time.LocalDateTime" ->
+        }
+      }
+      case "java.time.LocalDateTime" -> {
+        if (nullable) {
+          out.printf(
+              "json.put(\"%s\", null == o.%s() ? null :"
+                  + " o.%s().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));%n",
+              name, name, name);
+        } else {
           out.printf(
               "json.put(\"%s\", o.%s().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));%n",
               name, name);
-      case "java.time.OffsetDateTime" ->
+        }
+      }
+      case "java.time.OffsetDateTime" -> {
+        if (nullable) {
+          out.printf(
+              "json.put(\"%s\", null == o.%s() ? null :"
+                  + " o.%s().format(DateTimeFormatter.ISO_OFFSET_DATE_TIME));%n",
+              name, name, name);
+        } else {
           out.printf(
               "json.put(\"%s\", o.%s().format(DateTimeFormatter.ISO_OFFSET_DATE_TIME));%n",
               name, name);
+        }
+      }
       default -> throw new GenerationException("Unsupported class: " + className);
     }
   }
 
-  private static void iterableToJson(PrintWriter out, String name, String className) {
+  private static void iterableToJson(
+      PrintWriter out, String name, String className, boolean nullable) {
+
+    if (nullable) {
+      out.printf("if (null != o.%s()) {%n", name);
+    }
+
     out.printf("JsonArray %s = new JsonArray();%n", name);
     out.printf("for (var i : o.%s()) {%n", name);
 
@@ -76,6 +108,11 @@ final class ToJsonGenerator {
 
     out.println("}");
     out.printf("json.put(\"%s\", %s);%n", name, name);
+
+    if (nullable) {
+      out.println("}");
+    }
+
     out.println();
   }
 }
